d6ce376254a49f18f1a69554669ddb117865f2ca
Rohak Singhal <Rohak Singhal>
2016-09-30 17:17:22 +0800

    rpi py

---
+++ Raspberry Pi/Python/emal.py
@@ -0,0 +1,19 @@
+import smtplib
+GMAIL_USER = 'rohaksinghal14@gmail.com'
+GMAIL_PASS = 'dunlop691'
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 587
+
+def send_email(recipient, subject, text):
+	smtpserver = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+	smtpserver.ehlo()
+	smtpserver.starttls()
+	smtpserver.ehlo
+	smtpserver.login(GMAIL_USER, GMAIL_PASS)
+	header = 'To:' + recipient + '\n' + 'From: ' + GMAIL_USER
+	header = header + '\n' + 'Subject:' + subject + '\n'
+	msg = header + '\n' + text + ' \n\n'
+	smtpserver.sendmail(GMAIL_USER, recipient, msg)
+	smtpserver.close()
+
+send_email('rohaksinghal14@gmail.com', 'sub', 'this is text')
---
+++ Raspberry Pi/Python/srl.py
@@ -0,0 +1,11 @@
+import serial
+import requests
+
+port = "/dev/ttyACM0"
+
+serialFromArduino = serial.Serial(port,9600)
+serialFromArduino.flushInput()
+while True:
+	if (serialFromArduino.inWaiting() > 0):
+		input = serialFromArduino.read(1)
+		print(ord(input))
