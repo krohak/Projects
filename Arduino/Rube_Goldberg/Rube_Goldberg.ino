7c175e804bf589723e1c41e55a440bddbeee5101
Rohak <rohak@localhost.localdomain>
2016-11-22 02:36:39 +0800

    yo

--- Arduino/Rube_Goldberg/Rube_Goldberg.ino
+++ Arduino/Rube_Goldberg/Rube_Goldberg.ino
@@ -1,8 +1,8 @@
 /***********************************
- * 
+ *
  * The code for a Rube Goldberg machine built for the final project of my Intro to
  * Electrical and Electronics Engineering course at The University of Hong Kong.
- * 
+ * hi
  * @krohak
  *********************************/
 #include <Servo.h>
@@ -11,7 +11,7 @@ int motorstage1=11;
 int solenoidstage3=8;
 int stage5motor=12;
 
-Servo Stage2; 
+Servo Stage2;
 Servo Stage3;
 
 int led4=7;
@@ -21,39 +21,39 @@ int led3=5;
 int bc=A0;
 
 void setup() {
-  
+
   pinMode(8,1);//Solenoid st3
-  
-  Stage2.attach(9); 
+
+  Stage2.attach(9);
   Stage2.write(90);
   Stage3.attach(10);
   Stage3.write(90);
-  
+
   pinMode(12,1);
    pinMode(11,1);//Motor Stage 1
    pinMode(13,1);
-   
+
    Serial.begin(9600);
-   
+
    pinMode(4,1);
    pinMode(5,1);
    pinMode(6,1);
    pinMode(7,1);
-   
+
    digitalWrite(led1,1);
-   
+
    Serial.begin(9600);
-   
+
 }
 
 void loop() {
   Serial.println(analogRead(A0));
- 
+
   if((analogRead(A0))<500)
   {
 
-/************************STAGE 1********************************************************/    
-  
+/************************STAGE 1********************************************************/
+
  Serial.println("BEGIN");
   digitalWrite(motorstage1, 1);
   delay(12500);
@@ -61,7 +61,7 @@ void loop() {
   Serial.println("s1done");
 
 /************************STAGE 2********************************************************/
- 
+
    delay(5000);
   Stage2.write(105);
   delay(700);
@@ -73,9 +73,9 @@ void loop() {
   delay(10);
   Serial.println("s2done");
 
-  
- /************************STAGE 3********************************************************/ 
- 
+
+ /************************STAGE 3********************************************************/
+
   delay(2000);     //20.200
   Stage3.write(75);
    delay(18000);
@@ -93,20 +93,20 @@ void loop() {
   delay(500);
   delay(500);
   */
-  
+
   Stage3.write(90);
   delay(1000);
- 
+
   /////////////////////
-  
+
   digitalWrite(solenoidstage3, 0);
   (3000);
 
   /************************STAGE 4********************************************************/
-  
+
     digitalWrite(led1,1);
   delay(1000);
-  
+
   digitalWrite(led2,1);
   delay(500);
   digitalWrite(led1,0);
@@ -136,10 +136,10 @@ void loop() {
   delay(18250);
   Stage3.write(90);
   delay(1000);
-  
+
   delay(10000000);
-  
-  
+
+
   ////
   }
 
